614712087b14c627327f5fb7782504c7ec816066
Umid2003 <karl_karl_2020@inbox.ru>
2025-03-24 22:31:46 +0500

    class added

--- src/data.js
+++ src/data.js
@@ -12,9 +12,9 @@ export const onlineCourses=[
     { title: 'JavaScript', length: '3 months', level: 'Intermediate', price: 20, img: FaJs  },
     { title: 'React JS', length: '4 months', level: 'Intermediate', price: 35, img: FaReact  },
     { title: 'Vue JS', length: '4 months', level: 'Beginner', price: 30, img: FaVuejs  },
-    { title: 'Angular', length: '3 months', level: 'Advanced', price: 33, img: FaAngular  },
+    { title: 'Angular JS', length: '3 months', level: 'Advanced', price: 33, img: FaAngular  },
     { title: 'Node JS', length: '2 months', level: 'Intermediate', price: 25, img: FaNodeJs },
-    { title: 'Bot', length: '1.5 months', level: 'Beginner', price: 15, img: FaTelegramPlane },
+    { title: 'Telegram Bot', length: '1.5 months', level: 'Beginner', price: 15, img: FaTelegramPlane },
     { title: 'React Native', length: '4 months', level: 'Advanced', price: 40, img: FaReacteurope  },
     { title: 'Python', length: '3 months', level: 'Intermediate', price: 27, img: FaPython  },
     { title: 'TypeScript', length: '1 month', level: 'Beginner', price: 15, img: SiTypescript  }
